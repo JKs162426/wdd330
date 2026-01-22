0a1f16189b16001871204852a02b870303df11d2
Paper-Dad <broberts@byupathway.edu>
2026-01-21 20:34:11 -0700

    Completed Week 2 team activity

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,4 +1,6 @@
-import { getLocalStorage } from "./utils.mjs";
+import { getLocalStorage, loadHeaderFooter } from "./utils.mjs";
+
+loadHeaderFooter();
 
 const cartFooter = document.querySelector(".cart-footer");
 const cartTotalEl = document.querySelector(".cart-total");
